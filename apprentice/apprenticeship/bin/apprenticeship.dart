f30527480a14b7f1c2772ee032b6bd140064d14c
bethel-m <bethelcletus87@gmail.com>
2023-04-19 16:57:31 +0100

    classes

--- apprentice/apprenticeship/bin/apprenticeship.dart
+++ apprentice/apprenticeship/bin/apprenticeship.dart
@@ -1,71 +1,35 @@
-import 'dart:math';
-import 'package:characters/characters.dart';
-import 'package:apprenticeship/apprenticeship.dart' as apprenticeship;
-import 'package:test/test.dart';
-
-enum hl {
-  lsd,
-  sdkls,
-}
 void main() {
-  var lss = hl.lsd;
-  print(lss.index);
-  print(hl);
-switch (lss){
-  case hl.lsd:
-  print("object");
-  break;
-  case hl.sdkls:
-  print("object");
-  break;
-}
-  print('Hello world: ${apprenticeship.calculate()}!');
-  jump();
-
-  var attendance = (90 * 20)/100;
-  var homework = (80 * 30)/100;
-  var exam = (80 * 50)/100;
-  final grade = exam + homework + attendance;
-  print("attendance: $attendance%,homework: $homework%,exam :$exam%");
-  print("grade ${grade.toInt()}");
-
-  const rom = "🇷🇴";
-  const chad = "🇹🇩";
-print("objec------------------------------t");
-  for (var codepoint in rom.runes){
-    print(String.fromCharCode(codepoint));
-  }
-print(rom);
-print("objec------------------------------t");
-  for (var codepoint in rom.codeUnits){
-    print(String.fromCharCode(codepoint));
-    print(codepoint);
-  }
-print("objec------------------------------t");
-  for (var codepoint in rom.characters){
-    print(codepoint);
-  }
-
-  print(rom.codeUnits);
-  print("${rom.runes} is romania");
-  print("${chad.runes} is chad");
-  print(chad.codeUnits);
-
-  const vote = "Thumbs up! 👍🏿";
-  print(vote.codeUnits);
-  print(vote.runes);
-  print(vote.characters.length);
-  const value = 10 / 2;
-  const number = 10;
-const multiplier = 5;
- final summary = '$number \u00D7 $multiplier = ${number * multiplier}';
- print(summary);
+  final bert =  Student("bert","ernie",3);
+  print(bert);
+  var spe = Sphere(12);
+  print(spe.area);
+  print(spe.volume);
 }
 
-// just a comment for jumping
-///documentation for jump
-jump() {
-  print("object");
-  print(4 / 2);
-  print(5 ~/ 4);
+class Student{
+  final String firstName;
+  final String lastName;
+  int grade;
+  Student(this.firstName,this.lastName,this.grade);
+  @override
+  String toString() {
+    // TODO: implement toString
+    return "$firstName scored $grade";
+  }
+}
+
+class Sphere{
+  //A=4πr2 V=4/3(πr3)
+  static const double pi= 3.14159265359;
+  const Sphere(this.radius)
+  : assert(radius>=0);
+  final double radius ;
+  double get volume{
+    double v = (4/3)*pi*radius*radius*radius;
+    return v;
+  }
+  double get area{
+    double a = 4 * pi * radius * radius;
+    return a;
+  }
 }
---
+++ apprentice/apprenticeship/bin/user.dart
@@ -0,0 +1,17 @@
+class User{ 
+int _id ;
+int get id => _id;
+int get(){
+  return 23;
+}
+User({id = 0})
+:_id=id;
+ User.an() : this();
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return "zzzzzzzzzzzzzzzzzz $id";
+  }
+
+}
