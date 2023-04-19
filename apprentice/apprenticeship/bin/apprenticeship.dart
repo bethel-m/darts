69ec11633198a0805e0b6b1c5adbe7e173ce9085
bethel-m <bethelcletus87@gmail.com>
2023-04-19 23:49:42 +0100

    collections:

--- apprentice/apprenticeship/bin/apprenticeship.dart
+++ apprentice/apprenticeship/bin/apprenticeship.dart
@@ -1,39 +1,51 @@
-import 'dart:math';
-import 'package:characters/characters.dart';
-import 'package:apprenticeship/apprenticeship.dart' as apprenticeship;
-import 'package:test/test.dart';
 
-enum hl {
-  lsd,
-  sdkls,
-}
 void main() {
-  print('Hello world: ${apprenticeship.calculate()}!');
-  int result = mood("bad")??0;
+const para = 'Once upon a time there was a Dart programmer who '
+    'had a challenging challenge to solve. Though the challenge was great, '
+    'a solution did come. The end.';
+  print(unique(para));
+  print(count(para));
+  var user1 = User(1,"bob");
+  var user2 = User(2,"grace");
+  var user3 = User(3,"tam");
+  var userList = [user1,user2,user3];
+  print(conv(userList));
 }
 
-int? mood(String mood){
-  if(mood=="good"){
-    return 42;
+Set unique(String text){
+  Set<String> un = {};
+  var all = text.split(" ");
+  all.forEach((element) { 
+    un.add(element);
+    }
+  );
+  return un;
+}
+Map<String,int> count(String text){
+  Map<String,int> vals = {};
+  var all = text.split(" ");
+  for (String word in all){
+    if (vals.containsKey(word)){
+      vals[word]=vals[word]!+1;
+    }else{
+      vals[word]=1;
+    }
   }
-  return null;
+  return vals;
 }
 
-class Name{
-  String givenName;
-  String? surname;
-  bool surnameIsFirst;
-  Name(this.givenName,this.surname,this.surnameIsFirst);
+class User{
+  int id;
+  String name;
+  User(this.id,this.name);
 
-  @override
-  String toString() {
-    // TODO: implement toString
-    if (surname==null){
-      return givenName;
-    }
-    if (surnameIsFirst){
-      return "$surname $givenName";
-    }
-    return "$givenName $surname";
+}
+List<Map<String,String>> conv(List<User> users){
+  var usermap = <Map<String,String>>[];
+  for (var user in users){
+    Map<String,String> umap={};
+    umap[user.id.toString()]=user.name;
+    usermap.add(umap);
   }
+  return usermap;
 }
