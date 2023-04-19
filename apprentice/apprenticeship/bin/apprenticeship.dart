b25cfaa07e42499ec0cb21cbd4dd0bdea8ced578
bethel-m <bethelcletus87@gmail.com>
2023-04-19 20:05:43 +0100

    null safety

--- apprentice/apprenticeship/bin/apprenticeship.dart
+++ apprentice/apprenticeship/bin/apprenticeship.dart
@@ -9,60 +9,31 @@ enum hl {
 }
 void main() {
   print('Hello world: ${apprenticeship.calculate()}!');
-  jump();
+  int result = mood("bad")??0;
+}
 
-  var number = 946;
-  var n = 0;
-  while(true){
-    if (pow(2,n)>= number){
-      print("found it");
-      print("its 2 raise power of $n");
-      break;
+int? mood(String mood){
+  if(mood=="good"){
+    return 42;
+  }
+  return null;
+}
+
+class Name{
+  String givenName;
+  String? surname;
+  bool surnameIsFirst;
+  Name(this.givenName,this.surname,this.surnameIsFirst);
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    if (surname==null){
+      return givenName;
     }
-    n++;
+    if (surnameIsFirst){
+      return "$surname $givenName";
+    }
+    return "$givenName $surname";
   }
-
-  var nth=1;
-  var n1 = 1;
-  var n2 = 1;
-  var nv;
-  if (nth == 0 || nth == 1){
-    print("the $nth fibonacci is 1");
-  }
-if (nth > 2){  for (int i=2;i < nth;i++){
-     nv = n1 + n2;
-     n1 =n2;
-     n2 = nv;
-
-  }
-  print("the $nth fibonacci number is $nv");
-  }
-
-
-  for(int i = 10;i >= 0; i--){
-    print(i);
-  }
-
-  for (int i = 0; i <= 10; i++){
-  print(i/10);
-}
-
-var func = (int jds){
-  return 423;
-};
-
-const numbers = [3,4,5,7];
-numbers.forEach((element) {
-  final tripled = element * 3;
-  print(tripled);
-});
-}
-
-
-// just a comment for jumping
-///documentation for jump
-jump() {
-  print("object");
-  print(4 / 2);
-  print(5 ~/ 4);
 }
