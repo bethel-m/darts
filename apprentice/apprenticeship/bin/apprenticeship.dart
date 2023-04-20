64fb5e183349fe755b90eb6da65e0faa1a7e1e25
bethel-m <bethelcletus87@gmail.com>
2023-04-20 12:18:14 +0100

    advanced classes

--- apprentice/apprenticeship/bin/apprenticeship.dart
+++ apprentice/apprenticeship/bin/apprenticeship.dart
@@ -1,51 +1,81 @@
 
 void main() {
-const para = 'Once upon a time there was a Dart programmer who '
-    'had a challenging challenge to solve. Though the challenge was great, '
-    'a solution did come. The end.';
-  print(unique(para));
-  print(count(para));
-  var user1 = User(1,"bob");
-  var user2 = User(2,"grace");
-  var user3 = User(3,"tam");
-  var userList = [user1,user2,user3];
-  print(conv(userList));
+var p1 = Platypus(23);
+var p2 = Platypus(36);
+var p3 = Platypus(89);
+var p4 = Platypus(90);
+print(p1.compareTo(p2));
+List pl = [p1,p2,p3,p4];
+pl.sort();
+print(pl);
+
+var data1 = DataRepository(23);
+print(data1);
+print(data1.runtimeType);
+print(data1.numberOfTasks());
+
+var kls= Duration();
+var m = 2.minutes;
+print(m);
 }
 
-Set unique(String text){
-  Set<String> un = {};
-  var all = text.split(" ");
-  all.forEach((element) { 
-    un.add(element);
-    }
-  );
-  return un;
-}
-Map<String,int> count(String text){
-  Map<String,int> vals = {};
-  var all = text.split(" ");
-  for (String word in all){
-    if (vals.containsKey(word)){
-      vals[word]=vals[word]!+1;
-    }else{
-      vals[word]=1;
-    }
+
+class Platypus implements Comparable{
+  final int wieght;
+
+  layEggs(){
+    print("plob plob");
   }
-  return vals;
-}
-
-class User{
-  int id;
-  String name;
-  User(this.id,this.name);
-
-}
-List<Map<String,String>> conv(List<User> users){
-  var usermap = <Map<String,String>>[];
-  for (var user in users){
-    Map<String,String> umap={};
-    umap[user.id.toString()]=user.name;
-    usermap.add(umap);
+  eat(){
+    print("munch munch");
   }
-  return usermap;
+  Platypus(this.wieght);
+  
+  @override
+  int compareTo(other) {
+      if (wieght > other.wieght){
+        return 1;
+      }else{
+        if (wieght < other.wieght){
+          return -1;
+        }
+      }
+    return 0;
+    //throw UnimplementedError();
+  }
+  @override
+  String toString() {
+    // TODO: implement toString
+    return "i weigh $wieght";
+  }
+}
+
+
+
+abstract class DataRepository{
+  int numberOfTasks(){
+    return 34;
+  }
+  factory DataRepository(int taks){
+    return Mockdata(taks);
+  }
+}
+
+class Mockdata implements DataRepository{
+  int userTasks;
+  Mockdata(this.userTasks);
+  @override
+  int numberOfTasks() {
+    return userTasks;
+  }
+  @override
+  String toString() {
+    // TODO: implement toString
+    return "user has $userTasks tasks";
+  }
+}
+
+
+extension on int{
+  Duration get minutes => Duration(minutes: this);
 }
